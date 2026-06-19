bc776b5b50aecc951aea2e3a6f8367ed25a65957
ritanya01 <ritanya70@gmail.com>
2026-06-19 10:40:58 +0530

    Update declaringvar.cpp

--- declaringvar.cpp
+++ declaringvar.cpp
@@ -1,1 +1,24 @@
+using namespace std;
+#include <iostream>
 
+int main() {
+    string name;
+    int    age;
+    float height;
+    float salary;
+
+    cout << "Enter your name:";
+    cin >> name;
+    cout << "Enter your age:"; 
+    cin >> age;
+    cout << "Enter your height:"; 
+    cin >> height;
+    cout << "Enter your salary:"; 
+    cin >> salary;
+
+    cout << "Name:   " << name   << "\n";
+    cout << "Age:    " << age    << "\n";
+    cout << "Height: " << height << "\n";
+    cout << "Salary: " << salary << "\n";
+    return 0;
+}
