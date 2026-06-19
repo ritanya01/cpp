c8fa2953628fc2b02ab4e984e67a34ceb1d7c071
ritanya01 <ritanya70@gmail.com>
2026-06-19 12:34:21 +0530

    Create digitinones.cpp

---
+++ digitinones.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+int main(){
+    int num,digit;
+    cout<<"Enter a number:";
+    cin>>num;
+    digit=num%10;
+    cout<<"The digit in one's place:"<<digit;
+    return 0;
+}
