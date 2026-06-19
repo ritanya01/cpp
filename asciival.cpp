a7d7cc147c608ac7f40a0e63592b5dbb91d9e058
ritanya01 <ritanya70@gmail.com>
2026-06-19 12:23:05 +0530

    Create asciival.cpp

---
+++ asciival.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+int main(){
+    char ch;
+    cout<<"Enter any char:";
+    cin>>ch;
+    cout<<"The ascii val of"<<ch<<"is:"<<(int)ch;
+    return 0;
+    
+}
