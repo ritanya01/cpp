11d09dd604d4c7032936709cb63fd40b9656be94
ritanya01 <ritanya70@gmail.com>
2026-06-19 10:59:15 +0530

    Create evenorodd.cpp

---
+++ evenorodd.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <iostream>
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if (n%2==0)
+        cout<<n<<" is even"<<"\n";
+    else
+        cout<<n<<" is odd"<<"\n";
+    return 0;
+}
