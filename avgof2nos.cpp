cdb4c635a83c1818dc05fd0c84e714b12304bf4f
ritanya01 <ritanya70@gmail.com>
2026-06-19 12:50:24 +0530

    Create avgof2nos.cpp

---
+++ avgof2nos.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include<iostream>
+int main(){
+    int num1,num2;
+    float avg;
+    cout<<"Enter num1:";
+    cin>>num1;
+    cout<<"Enter num2:";
+    cin>>num2;
+    avg=(num1+num2)/2;
+    cout<<"The avg of 2 nos:"<<avg;
+    return 0;
+}
