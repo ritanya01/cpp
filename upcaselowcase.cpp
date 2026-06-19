cbfe715b23b2a6345bff23186712c3f48d1aca06
ritanya01 <ritanya70@gmail.com>
2026-06-19 12:31:28 +0530

    Create upcaselowcase.cpp

---
+++ upcaselowcase.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+#include<iostream>
+int main(){
+    char ch;
+    cout<<"Enter a upper case char:";
+    cin>>ch;
+    cout<<"The char in lower case is"<<(char)tolower(ch);
+    return 0;
+}
