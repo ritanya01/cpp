c7267d0a2ae6a048fbbdcbaecf2b3aeb3cea53af
ritanya01 <ritanya70@gmail.com>
2026-06-19 10:51:21 +0530

    Create areaofrect.cpp

---
+++ areaofrect.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include <iostream>
+int main(){
+    float length, width;
+    cout<<"Enter length:"; 
+    cin>>length;
+    cout<<"Enter width:";
+    cin>>width;
+    float area=length*width;
+    cout<<"Area of rectangle:"<<area<<"\n";
+    return 0;
+}
