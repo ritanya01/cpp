5cdd7934cf62f12ac6f24bd3833632ceeaaa0d2f
ritanya01 <ritanya70@gmail.com>
2026-06-19 12:27:47 +0530

    Create areaofcircle.cpp

---
+++ areaofcircle.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include<iostream>
+int main(){
+    float radius,area;
+    cout<<"Enter a radius:";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"The area of the circle:"<<area;
+    return 0;
+}
