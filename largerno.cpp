c22ec77906d8245a87cd282f90e4365cff5b2f3f
ritanya01 <ritanya70@gmail.com>
2026-06-19 13:16:49 +0530

    Create largerno.cpp

---
+++ largerno.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+#include<iostream>
+int main(){
+    int num1,num2;
+    cout<<"Enter two nos:";
+    cin>>num1>>num2;
+    if (num1>num2)
+        cout<<"The larger no is:"<<num1;
+    else
+       cout<<"The larger no is:"<<num2;
+    return 0;
+}
